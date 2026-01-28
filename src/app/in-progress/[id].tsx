750a32d115a418a273830d03f20f042a8e5c7341
Gabriel <fco.gabrielsilva2408@gmail.com>
2026-01-28 17:41:26 -0300

    Aula 76 - Criando a rota In Progress

---
+++ src/app/in-progress/[id].tsx
@@ -0,0 +1,16 @@
+import { View, Text, Button } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+
+export default function InProgress() {
+  const params = useLocalSearchParams<{ id: string }>();
+
+  return (
+    <View
+      style={{ flex: 1, justifyContent: "center", backgroundColor: "white" }}
+    >
+      <Text>ID: {params.id}</Text>
+
+      <Button title="Voltar" onPress={() => router.back()} />
+    </View>
+  );
+}
